edc42ba693d0e83dd5f233c0fa40bc14f21592fb
letziegh <148257470+letziegh@users.noreply.github.com>
2024-04-26 14:04:43 -0400

    Initial Commit for Assignment 7

---
+++ src/com/coderscampus/CustomArrayList.java
@@ -0,0 +1,51 @@
+package com.coderscampus;
+
+
+    public class CustomArrayList<T> implements CustomList<T> {
+
+        Object[] items = new Object[10];
+        int index = 0;
+
+
+
+
+
+        @Override
+        public boolean add(T item) {
+            if(item==null) {
+
+                return false;
+            }
+
+
+            System.out.println(index);
+            if (items.length == index) {
+                items = Arrays.copyOf(items, items.length*2);
+                items[index]= item;
+                index++;
+            }else {
+                items[index]= item;
+                index++;
+            }
+
+
+            return true;
+        }
+
+        @Override
+        public int getSize() {
+            int itemSize = items.length;
+            //	System.out.println("Number of items in the array: " + itemSize);
+            return itemSize;
+        }
+
+        @Override
+        public T get(int index) {
+
+            return (T) items[index];
+
+        }
+
+    }
+
+
