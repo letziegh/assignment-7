643dc255727a93c2620b90f33e022ec271f3df9f
letziegh <148257470+letziegh@users.noreply.github.com>
2024-05-22 13:39:42 -0400

    Final commit 3 to Assignment 7

--- test/com/coderscampus/CustomArrayListTest.java
+++ test/com/coderscampus/CustomArrayListTest.java
@@ -13,6 +13,17 @@ class CustomArrayListTest {
         assertThrows(IndexOutOfBoundsException.class, () -> customArrayList.get(-1));
     }
     @Test
+    public void testGet_GivenValidIndex_ThenReturnCorrectItem() {
+        CustomArrayList<String> customArrayList = new CustomArrayList<>();
+        customArrayList.add("item1");
+        customArrayList.add("item2");
+        customArrayList.add("item3");
+
+        String actualItem = customArrayList.get(1);
+
+        assertEquals("item2", actualItem);
+    }
+    @Test
     public void test_Remove_Item_A() {
         CustomArrayList<String> list = new CustomArrayList<>();
         list.add("A");
@@ -23,12 +34,48 @@ class CustomArrayListTest {
         assertEquals("A", removedItem);
         assertEquals(1, list.getSize());
     }
+    //add item
     @Test
     public void test_Add_Item() {
         CustomArrayList<String> list = new CustomArrayList<>();
         assertTrue(list.add("Item 1"));
         assertEquals(1, list.getSize());
         assertEquals("Item 1", list.get(0));
+    }
+    @Test
+    public void testAddMethod_AddingNull() {
+        CustomArrayList<String> list = new CustomArrayList<>();
+        boolean result = list.add(null);
+
+        // Verifying that null is not added to the list
+        assertFalse(result);
+        assertEquals(0, list.getSize());
+    }
+
+    @Test
+    public void testAddMethod_ArrayExpansion() {
+        CustomArrayList<String> list = new CustomArrayList<>();
+
+        // Adding more than 10 items to the list to force an array expansion
+        for (int i = 1; i <= 11; i++) {
+            list.add("testItem" + i);
+        }
+
+        // Verifying that all items were successfully added
+        assertEquals(11, list.getSize());
+        assertEquals("testItem11", list.get(10));
+    }
+    @Test
+    public void addAtIndexMethodTest() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+
+        list.add(0, 10);
+        list.add(1, 20);
+        list.add(1, 15);
+        assertEquals(3, list.getSize());
+        assertEquals(10, list.get(0));
+        assertEquals(15, list.get(1));
+        assertEquals(20, list.get(2));
     }
 
     /**
@@ -51,6 +98,13 @@ class CustomArrayListTest {
         int result = list.getSize();
         assertEquals(2, result, "Expected size after adding two elements to be 2");
     }
+    @Test
+    void getSize_inEmptyList_returnsZero() {
+        CustomList<Object> list = new CustomArrayList<>();
+        int size = list.getSize();
+        assertEquals(0, size, "The size of an empty list should be 0.");
+    }
+
 
 
 
